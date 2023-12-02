3efba0fe2790aca6b42ff482ef4a18387c6045e7
dphill10 <derekp@uark.edu>
2023-12-02 14:49:34 -0600

    Initial commit for superhero API

---
+++ Group 1 Superhero View/ticket_system.py
@@ -0,0 +1,41 @@
+import requests
+import json
+from utils import text_print_utils as utils
+from utils.text_print_options import PrintOptions, Term
+from views import technician_view, ticket_view, user_view, organization_view, department_view
+
+api_url_base = "http://localhost:5000/"
+
+index = 0
+
+menu_options = [#"1. Tickets", "2. Technicians", "3. Users", "4. Organizations", "5. Departments", 
+                "0. Quit"]
+
+def run_system():
+    utils.print_text_block(header="Group 1's Superhero API Viewer", text="Main Menu", bottom_border=False)
+
+    while True:
+        utils.print_text_block("Options:", menu_options)
+        user_input = input("Enter a command: ")
+
+        if user_input == "1":
+            pass
+            # ticket_view.run(api_url_base)
+        # elif user_input == "2":
+            # technician_view.run(api_url_base)
+        # elif user_input == "3":
+            # user_view.run(api_url_base)
+        # elif user_input == "4":
+            # organization_view.run(api_url_base)
+        # elif user_input == "5":
+            # department_view.run(api_url_base)
+
+        elif user_input == "0":
+            print("Exiting the system.")
+            break
+        else:
+            print("Invalid command. Please try again.")
+
+        utils.print_text_block("Main Menu", top_border=True, bottom_border=False)
+
+run_system()
---
+++ Group 1 Superhero View/utils/text_print_utils.py
@@ -0,0 +1,162 @@
+import os
+import textwrap
+from textwrap import wrap
+from tabulate import tabulate
+from .text_print_options import PrintOptions, Term
+
+def print_text(args = "", options: PrintOptions = None):
+    if options is None:
+        options = PrintOptions()
+
+    # Validate alignment input
+    if options.alignment not in ['center', 'left', 'right']:
+        raise ValueError("alignment must be 'center', 'left', or 'right'")
+
+    if not isinstance(args, list) and not isinstance(args, str):
+        raise TypeError("text must be a string or a list of strings")
+
+    def align_text(text, available_width, alignment):
+        # Add the specified number of spaces to the front of the text
+        # text = ' ' * (options.tab_spaces * 4) + text
+
+        # if alignment == 'center':
+        #     return text.center(available_width)
+        # elif alignment == 'left':
+        #     return text.ljust(available_width)
+        # elif alignment == 'right':
+        #     return text.rjust(available_width)
+        
+        # Subtract the tab spaces from the available width
+        available_width -= options.tab_spaces * 4
+
+        if alignment == 'center':
+            spaces_before = ' ' * ((available_width - len(text)) // 2)
+            spaces_after = ' ' * (available_width - len(text) - len(spaces_before))
+            return Term.RESET + ' ' * (options.tab_spaces * 4) + spaces_before + options.text_color + text.strip() + Term.RESET + spaces_after
+        elif alignment == 'left':
+            spaces_after = ' ' * (available_width - len(text))
+            return Term.RESET + ' ' * (options.tab_spaces * 4) + options.text_color + text.lstrip() + Term.RESET + spaces_after
+        elif alignment == 'right':
+            spaces_before = ' ' * (available_width - len(text))
+            return Term.RESET + ' ' * (options.tab_spaces * 4) + spaces_before + options.text_color + text.strip() + Term.RESET
+
+
+    if isinstance(args, str):
+        lines = textwrap.wrap(args, options.screen_width - 2)
+        if len(lines) == 0:
+            print(options.border_marker_color + options.border_marker_char + options.text_color + " " * (options.screen_width - 2) + options.border_marker_color + options.border_marker_char + options.text_color)
+        else:
+            for line in lines:
+                aligned_text = align_text(line, options.screen_width - 2, options.alignment)
+                print(options.border_marker_color + options.border_marker_char + options.text_color + aligned_text + options.border_marker_color + options.border_marker_char + options.text_color)
+        return
+
+
+    if isinstance(args, list):
+        available_width = options.screen_width - 2
+        # Start with an empty line
+        current_line = ""
+        for i, item in enumerate(args):
+            # Add divider if not the first item in the line
+            if current_line:
+                item_with_divider = " " + options.item_divider_char + " " + item
+            else:
+                item_with_divider = item
+
+            # Check if the item fits in the current line
+            if len(current_line) + len(item_with_divider) <= available_width:
+                current_line += item_with_divider
+            else:
+                aligned_text = align_text(current_line, available_width, options.alignment)
+                print(options.border_marker_color + options.border_marker_char + options.text_color + aligned_text + options.border_marker_color + options.border_marker_char + options.text_color)
+               
+                # Start a new line with the current item
+                current_line = item
+
+        # Print any remaining text in the current line
+        if current_line:
+            aligned_text = align_text(current_line, available_width, options.alignment)
+            print(options.border_marker_color + options.border_marker_char + options.text_color + aligned_text + options.border_marker_color + options.border_marker_char + options.text_color)
+
+def print_divider(options: PrintOptions = None):
+    if options is None:
+        options = PrintOptions()
+    
+    text_print_options = options.copy()
+    text_print_options.text_color = options.line_divider_color
+
+    print_text(options.line_divider_char * (options.screen_width - 2), text_print_options)
+
+def print_blank_line(options: PrintOptions = None):
+    if options is None:
+        options = PrintOptions()
+    print_text(options=options)
+
+def print_text_block(header = "", text = "", footer = "", top_border = True, bottom_border = True, options: PrintOptions = None):
+    if options is None:
+        options = PrintOptions()
+
+    if top_border:
+        print_divider(options)
+
+    print_blank_line(options)
+
+    if header:
+        print_options = options.copy()
+        print_options.text_color = Term.BOLD
+        print_text(header, print_options)
+        print_blank_line(options)
+
+    if text:
+        print_text(text, options)
+
+    print_blank_line(options)
+
+    if footer:
+        print_text(footer, options)
+
+    if bottom_border:
+        print_divider(options)
+
+def get_input(prompt, options: PrintOptions = None):
+    if options is None:
+        options = PrintOptions()
+    
+    text_print_options = options.copy()
+    text_print_options.text_color = Term.YELLOW_BOLD
+
+    return input(options.text_color + prompt + text_print_options.text_color)
+
+def print_json_in_table_format(json_object, ordered_keys=None, rename_keys=None, tablefmt='fancy_grid', options: PrintOptions=None, max_value_width=None):
+    if options is None:
+        options = PrintOptions()
+
+    if max_value_width is None:
+        keys = ordered_keys if ordered_keys else json_object.keys()
+        if rename_keys:
+            keys = [rename_keys.get(key, key) for key in keys]
+        max_key_width = max(len(key) for key in keys) + 2
+        max_value_width = options.screen_width - max_key_width - 7
+
+    json_object = {key: textwrap.fill(value, max_value_width) if isinstance(value, str) and len(value) > max_value_width else value for key, value in json_object.items()}
+
+    table = []
+    keys = ordered_keys if ordered_keys else json_object.keys()
+    for key in keys:
+        value = json_object.get(key)
+        if rename_keys and key in rename_keys:
+            key = rename_keys[key]
+        if value is not None and isinstance(value, dict):
+            for sub_key, sub_value in value.items():
+                if rename_keys and sub_key in rename_keys:
+                    sub_key = rename_keys[sub_key]
+                table.append([sub_key, sub_value])
+        else:
+            table.append([key, value])
+    table_string = tabulate(table, tablefmt=tablefmt)
+    
+    table_lines = table_string.split('\n')
+    
+    for line in table_lines:
+        print_text(line, options)
+
